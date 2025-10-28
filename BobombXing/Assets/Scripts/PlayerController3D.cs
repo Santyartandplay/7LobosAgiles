f8f81b2e488a83a19adec5df56254fd890b2cbd3
Santyartandplay <santyartandplay@gmail.com>
2025-10-27 19:17:25 -0500

    Añadí movimiento a Mario

---
+++ BobombXing/Assets/Scripts/PlayerController3D.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MarioMovimiento3D : MonoBehaviour
+{
+    public float velocidad = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        if (Input.GetKey(KeyCode.UpArrow)) vertical = 1f;
+        if (Input.GetKey(KeyCode.DownArrow)) vertical = -1f;
+        if (Input.GetKey(KeyCode.LeftArrow)) horizontal = -1f;
+        if (Input.GetKey(KeyCode.RightArrow)) horizontal = 1f;
+
+        Vector3 direccion = new Vector3(horizontal, 0, vertical).normalized;
+
+        // Se mueve si hay dirección
+        if (direccion.magnitude > 0)
+        {
+            Vector3 movimiento = direccion * velocidad * Time.deltaTime;
+            rb.MovePosition(transform.position + movimiento);
+
+            // Hace que gire en dirección al movimiento
+            transform.rotation = Quaternion.LookRotation(direccion);
+        }
+    }
+}
